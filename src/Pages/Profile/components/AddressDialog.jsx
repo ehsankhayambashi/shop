6707dbbe2d32e82291265d33731eab5e22259e45
ehsankhayambashi <khayambashi.ehsan@gmail.com>
2023-05-30 18:22:54 +0330

    7nd commit

--- src/Pages/Profile/components/AddressDialog.jsx
+++ src/Pages/Profile/components/AddressDialog.jsx
@@ -17,7 +17,7 @@ function AddressDialog({
   setShowForm,
 }) {
   return (
-    <Box display="flex" flexDirection="column" height="95vh">
+    <Box display="flex" flexDirection="column" height="90vh">
       <Box height="120px" px={2} pt={1} zIndex={2}>
         <AddressHeader handleCloseMap={handleCloseMap} />
       </Box>
